b9de9bb2e00bef2a3178555f1284f3f247329c4a
ishuangjin <ishuangjin@foxmail.com>
2022-08-17 15:59:19 +0800

    更改脚本实现逻辑

--- TSF接口测试/consumer-api.py
+++ TSF接口测试/consumer-api.py
@@ -6,7 +6,7 @@
 @QQ: 1525053461
 @Mail: ishuangjin@foxmail.com
 @Date: 2022-08-08 11:06:56
-@LastEditTime: 2022-08-16 10:49:25
+@LastEditTime: 2022-08-16 17:39:50
 @FilePath: \\Github\\MyScript\\TSF接口测试\\consumer-api.py
 @Copyright (c) 2022 by ishuangjin, All Rights Reserved.
 @Description: 测试tsf服务限流，脚本运行run_time秒，在每个unit_time内运行count次
@@ -17,7 +17,7 @@ import requests
 
 class TestUrl:
 
-    def __init__(self, host, port, url_merge, count, unit_time, run_time):
+    def __init__(self, host, port, url_merge, count, unit_time, run_time, tag_params=None):
         '''
         @description: 测试tsf服务限流，脚本运行run_time秒，在每个unit_time内运行count次
         @param  self: /
@@ -25,26 +25,27 @@ class TestUrl:
         @param  port: 端口
         @param  url_merge: url路径
         @param  count: 请求次数
-        @param  unit_time: 单位时间
-        @param  run_time: 程序运行时间
+        @param  unit_time: 单位时间(s)
+        @param  run_time: 程序运行时间(s)
         @return /
         '''
         self.host = host
         self.port = port
+        self.url_merge = url_merge
         self.count = count
         self.unit_time = unit_time
         self.run_time = run_time
-        self.url_merge = url_merge
+        self.tag_params = tag_params
+        self.url = 'http://{}:{}'.format(self.host, self.port) + self.url_merge
 
     def ping_url(self):
-        tag_params = {'tagName': 'user', 'tagValue': 'test'}
-        url = 'http://{}:{}'.format(self.host, self.port) + self.url_merge
-        re = requests.get(url=url, params=tag_params)
-        return print(re.text)
+        tag_params = self.tag_params
+        re = requests.get(url=self.url, params=tag_params)
+        return print(f'响应时间: {re.elapsed.total_seconds():.2f}s, 响应结果: {re.status_code}, 响应内容: {re.text}')
 
     def loop_ping(self):
         loop_num = self.run_time // self.unit_time  # 运行多少个循环
-        print(f"每{self.unit_time}秒运行{self.count}次，开始执行")
+        print(f"每{self.unit_time}秒运行{self.count}次，共运行{loop_num}个循环，开始执行！")
         for loop_n in range(loop_num):
             start_time = time.time()  # 获取开始时间
             start_time_format = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
@@ -52,20 +53,29 @@ class TestUrl:
             for _ in range(self.count):
                 self.ping_url()
             use_time = time.time() - start_time
-            sleep_time = self.unit_time - use_time
-            print(f"used {use_time:.2f}s")
-            print(f"watting {sleep_time:.2f}s ......\n")
-            time.sleep(sleep_time)
+            try:
+                sleep_time = self.unit_time - use_time
+                print(f"第{loop_n+1}次循环总耗时: {use_time:.2f}s")
+                print(f"等待: {sleep_time:.2f}s ......\n")
+            except ValueError:
+                sleep_time = 0
+                print(f"第{loop_n+1}次循环，总耗时: {use_time:.2f}s > 单位时间: {unit_time}s")
+                print("不等待直接开始下一个单位时间")
+            finally:
+                time.sleep(sleep_time)
         return print("done")
 
 
 if __name__ == '__main__':
-    host = '192.168.22.4'  # consumer 服务主机
-    port = '42247'  # consumer 端口
-    count = 10  # 请求数
-    unit_time = 3  # 单位时间，秒
-    run_time = 300  # run_time：脚本运行时间, 秒，需要跑多久脚本
-    url_merge = r'/jin-test1/global-ns/shenliufei-consumer-demo1/echo-rest/hello'  # 要访问的路径
-    test_url = TestUrl(host, port, url_merge, count, unit_time, run_time)
+    host = '192.168.22.4'
+    port = '42247'
+    # 每unit_time秒运行count次
+    count = 7
+    unit_time = 7
+    run_time = 300
+    tag_params = {'tagName': 'user', 'tagValue': 'test'}
+    url_merge = r'/jin-test1/global-ns/shenliufei-consumer-demo1/echo-rest/hello'  # consumer服务 api测试
+    # url_merge = r'/jin-test1/global-ns/shenliufei-provider-demo1/echo/hello'  # provider服务 api测试
+    test_url = TestUrl(host, port, url_merge, count, unit_time, run_time, tag_params)
     # test_url.ping_url()
     test_url.loop_ping()
