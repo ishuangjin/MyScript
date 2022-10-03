42ed27c23e8d337404ed87a080fdc12896643b33
ishuangjin <ishuangjin@foxmail.com>
2022-10-04 00:27:43 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# encoding:utf-8
+'''
+@Author: ishuangjin
+@WebSite: blog.ishuangjin.cn
+@QQ: 1525053461
+@Mail: ishuangjin@foxmail.com
+@Date: 2022-10-04 00:23:25
+@LastEditTime: 2022-10-04 00:23:29
+@FilePath: \\Git\\MyScript\\test.py
+@Copyright (c) 2022 by ishuangjin, All Rights Reserved.
+@Description: 
+'''
+from os import lseek
+
+lseek()
