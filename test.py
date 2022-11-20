e2f6353fabd702beaba3ecb17e1373486913a56f
ishuangjin <ishuangjin@foxmail.com>
2022-11-20 20:08:26 +0800

    TCTIPv6测试用例

--- test.py
+++ test.py
@@ -6,11 +6,22 @@
 @QQ: 1525053461
 @Mail: ishuangjin@foxmail.com
 @Date: 2022-10-04 00:23:25
-@LastEditTime: 2022-10-04 00:23:29
+@LastEditTime: 2022-10-04 00:35:11
 @FilePath: \\Git\\MyScript\\test.py
 @Copyright (c) 2022 by ishuangjin, All Rights Reserved.
 @Description: 
 '''
-from os import lseek
+'''
+原油：y，精炼油：j，氢气：h，煤炭：m，红糖：t，石墨：s
+'''
+y = "原油"
+j = "精炼油"
+h = "氢气"
+m = "煤炭"
+h = "红糖"
+s = "石墨"
 
-lseek()
+y = 2 * j + 1 * h
+j = 3 * h + s - 2 * h
+t = 2 * s + 2 * h
+m = 3 * j - h - 2 * j
